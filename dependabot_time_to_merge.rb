a065dd4f37cf5b4c80466e27f0f9a02cf250412f
Murilo Dal Ri <murilodalri@gmail.com>
2023-04-30 13:24:46 +0100

    wip

--- dependabot_time_to_merge.rb
+++ dependabot_time_to_merge.rb
@@ -16,8 +16,8 @@ class Dependabot
     @govuk_repos ||=
       JSON.parse(
         Net::HTTP.get(
-          URI('https://docs.publishing.service.gov.uk/repos.json')
-        )
+          URI("https://docs.publishing.service.gov.uk/repos.json"),
+        ),
       )
           .map { |repo| "alphagov/#{repo['app_name']}" }
   end
@@ -28,7 +28,19 @@ class Dependabot
 
     return nil if details.nil?
 
-    [details[1], details[2]]
+    from_version_parts = details[2].split(".")
+    to_version_parts = details[3].split(".")
+    update_type = nil
+
+    update_type = if from_version_parts[0] != to_version_parts[0]
+                    "major"
+                  elsif from_version_parts[1] != to_version_parts[1]
+                    "minor"
+                  else
+                    "patch"
+                  end
+
+    [details[1], details[2], update_type]
   end
 
   def get_repo_prs(repo, from)
@@ -53,7 +65,7 @@ class Dependabot
     repo_dependabot_prs = {}
 
     get_repo_prs(repo, from).each do |pr|
-      dependency_name, from_version = get_dependency_name_and_version(pr.title)
+      dependency_name, from_version, update_type = get_dependency_name_and_version(pr.title)
 
       next if dependency_name.nil?
 
@@ -64,27 +76,32 @@ class Dependabot
         closed_at: pr.closed_at,
         from_version: from_version,
         pr_number: pr.number,
+        update_type: update_type,
       }
     end
     repo_dependabot_prs
   end
 
-  def fetch_checks_status(repo, pr_number)
-    check_runs = client.check_runs_for_ref(repo, pr_number)
+  def fetch_checks_status(repo, commit_ref)
+    check_runs = client.check_runs_for_ref(repo, commit_ref)
     check_runs.check_runs.map(&:conclusion)
   end
 
   def get_repo_metrics(repo, from, to, outdated_limit)
     dependabot_prs = dependabot_history_per_repo(repo, from)
 
+    major_updates = 0
+    minor_updates = 0
+    patch_updates = 0
     total_opened_prs = 0
+    closed_without_merging = 0
     time_to_merge = []
     time_since_open = []
-    prs_per_dependency = Hash.new(0)
-    open_prs_per_dependency = Hash.new(0)
     outdated_dependencies = []
     long_merge_dependencies = []
     failing_prs = []
+    prs_per_dependency = Hash.new(0)
+    open_prs_per_dependency = Hash.new(0)
 
     dependabot_prs.each do |dependency, prs|
       opened_prs = prs.filter { |pr| pr[:created_at].between?(from, to) }
@@ -98,6 +115,15 @@ class Dependabot
         created_at = dependabot_prs[dependency]
                      .filter { |pr| pr[:from_version] == opened_pr[:from_version] }
                      .map { |pr| pr[:created_at] }.min
+
+        case opened_pr[:update_type]
+        when "patch"
+          patch_updates += 1
+        when "minor"
+          minor_updates += 1
+        when "major"
+          major_updates += 1
+        end
 
         if opened_pr[:closed_at].nil?
           days_since_open = (Date.today - created_at.to_date).to_i
@@ -115,6 +141,8 @@ class Dependabot
           failing_checks = check_conclusions.count { |conclusion| conclusion != "success" && conclusion != "neutral" && conclusion != "skipped" }
           failing_prs << { dependency: dependency, repo: repo, pr_number: pr_number } if failing_checks.positive?
         end
+
+        closed_without_merging += 1 if !opened_pr[:merged_at] && !opened_pr[:closed_at].nil?
 
         next unless opened_pr[:merged_at]
 
@@ -137,6 +165,10 @@ class Dependabot
       long_merge_dependencies: long_merge_dependencies,
       dependabot_history: dependabot_prs,
       failing_prs: failing_prs,
+      closed_without_merging: closed_without_merging,
+      major_updates: major_updates,
+      minor_updates: minor_updates,
+      patch_updates: patch_updates,
     }
   end
 
@@ -159,9 +191,11 @@ class Dependabot
     from = Time.parse(from)
     to = Time.parse(to)
     total_opened_prs = 0
-    closed_prs = 0
-    time_to_merge = []
-    time_since_open = []
+    merged_prs = 0
+    closed_without_merging = 0
+    major = 0
+    minor = 0
+    patch = 0
     prs_per_dependency = Hash.new(0)
     open_prs_per_dependency = Hash.new(0)
     frequently_updated = {}
@@ -169,14 +203,20 @@ class Dependabot
     long_merge_dependencies = []
     pr_data = []
     failing_prs = []
+    time_to_merge = []
+    time_since_open = []
 
     govuk_repos.each do |repo|
       repo_metrics = get_repo_metrics(repo, from, to, outdated_limit)
       total_opened_prs += repo_metrics[:total_opened_prs]
-      closed_prs += repo_metrics[:time_to_merge].size
+      merged_prs += repo_metrics[:time_to_merge].size
+      closed_without_merging += repo_metrics[:closed_without_merging]
       time_to_merge += repo_metrics[:time_to_merge]
       time_since_open += repo_metrics[:time_since_open]
       failing_prs += repo_metrics[:failing_prs]
+      major += repo_metrics[:major_updates]
+      minor += repo_metrics[:minor_updates]
+      patch += repo_metrics[:patch_updates]
 
       repo_metrics[:prs_per_dependency].each do |dependency, count|
         prs_per_dependency[dependency] += count
@@ -210,16 +250,22 @@ class Dependabot
     end
 
     time_to_merge_distribution = time_to_merge_distribution(time_to_merge)
-    pr_success_rate = success_rate(total_opened_prs, closed_prs)
+    pr_success_rate = success_rate(total_opened_prs, merged_prs + closed_without_merging)
     frequently_updated = frequently_updated.select { |_, count| count.positive? }.sort_by { |_, count| -count }.to_h
     prs_per_dependency = prs_per_dependency.select { |_, count| count.positive? }.sort_by { |_, count| -count }.to_h
     open_prs_per_dependency = open_prs_per_dependency.select { |_, count| count.positive? }.sort_by { |_, count| -count }.to_h
+
+    total_updates = major + minor + patch
+    major_update_percentage = (major.to_f / total_updates * 100).round(2)
+    minor_update_percentage = (minor.to_f / total_updates * 100).round(2)
+    patch_update_percentage = (patch.to_f / total_updates * 100).round(2)
 
     {
       from: from,
       to: to,
       total_opened_prs: total_opened_prs,
-      closed_prs: closed_prs,
+      merged_prs: merged_prs,
+      closed_without_merging: closed_without_merging,
       pr_success_rate: pr_success_rate,
       average_time_to_merge: (time_to_merge.sum(0.0) / time_to_merge.size).round(2),
       average_time_since_open: (time_since_open.sum(0.0) / time_since_open.size).round(2),
@@ -231,6 +277,9 @@ class Dependabot
       outdated_dependencies: outdated_dependencies,
       long_merge_dependencies: long_merge_dependencies,
       failing_prs: failing_prs,
+      major_update_percentage: major_update_percentage,
+      minor_update_percentage: minor_update_percentage,
+      patch_update_percentage: patch_update_percentage,
       pr_data: pr_data,
     }
   end
@@ -244,14 +293,20 @@ class Dependabot
       puts "Total PRs raised by Dependabot: #{metrics[:total_opened_prs]}"
       puts "  (The total number of pull requests opened by Dependabot during the specified time period)"
       puts ""
-      puts "Total PRs merged or closed: #{metrics[:closed_prs]}"
-      puts "  (The total number of pull requests either merged or closed during the specified time period)"
+      puts "% PRs raised by update type: major: #{metrics[:major_update_percentage]}%, minor: #{metrics[:minor_update_percentage]}%, patch: #{metrics[:patch_update_percentage]}%"
+      puts "  (The percentage of pull requests opened by Dependabot during the specified time period classified into major, minor and patch update type)"
+      puts ""
+      puts "Total PRs merged: #{metrics[:merged_prs]}"
+      puts "  (The total number of pull requests merged during the specified time period)"
+      puts ""
+      puts "Total PRs closed: #{metrics[:closed_without_merging]}"
+      puts "  (The total number of pull requests closed without merging during the specified time period)"
       puts ""
       puts "Total PRs still open: #{metrics[:time_since_open_count]}"
       puts "  (The total number of pull requests opened by Dependabot during the specified time period that are still open)"
       puts ""
       puts "PR success rate: #{metrics[:pr_success_rate].round(2)}%"
-      puts "  (The percentage of Dependabot PRs that have been successfully merged or closed)"
+      puts "  (The percentage of Dependabot PRs that have been successfully merged not counting PRs closed without)"
       puts ""
       puts "Average time to merge a PR (including superseded PRs): #{metrics[:average_time_to_merge]} days"
       puts "  (The average number of days it takes to merge a Dependabot PR, including those that were superseded by newer PRs)"
@@ -261,7 +316,7 @@ class Dependabot
       puts ""
       puts "-----------------------------------------------------------------"
       puts "#{metrics[:failing_prs].count} Failing PRs:"
-      puts metrics[:failing_prs].empty? ? "No PRs have failing checks" : metrics[:failing_prs].map { |item| "#{item[:repo]} - #{item[:dependency]} - PR ##{item[:pr_number]}" }
+      puts metrics[:failing_prs].empty? ? "" : metrics[:failing_prs].map { |item| "#{item[:repo]} - #{item[:dependency]} - PR ##{item[:pr_number]}" }
       puts "  (A list of open Dependabot PRs that currently have failing checks)"
       puts ""
       puts "-----------------------------------------------------------------"
